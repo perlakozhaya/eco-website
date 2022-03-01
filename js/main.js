93f7097ff912870ffef017fc8cefe761282c788c
Henry Kozhaya <henry.kozhaya@gmail.com>
2022-03-01 21:39:01 +0200

    Version: 1.0.0 - First Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = initMyPage;
+function initMyPage(){
+    // showAlert();    
+}
+function showAlert(){
+    var my_alert = document.getElementById("my_alert");
+    my_alert.classList.remove("d-none");
+}
