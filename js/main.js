e6779dd802697ed4492524a00a2e98cc94e48d90
Perla Kozhaya <kozhaya.perla@gmail.com>
2022-03-11 21:04:12 +0200

    Version: 1.1.0 - Beta Version. No JS

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = initMyPage;
-function initMyPage(){
-    // showAlert();    
-}
-function showAlert(){
-    var my_alert = document.getElementById("my_alert");
-    my_alert.classList.remove("d-none");
-}
