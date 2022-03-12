3a23f8fc527542fbadc4a3eae234a9c1ee40bc4e
Henry Kozhaya <henry.kozhaya@gmail.com>
2022-03-12 11:10:37 +0200

    Version: 1.1.2 - Header and Footer added by JS - Form submission message

--- js/main.js
+++ js/main.js
@@ -254,8 +254,23 @@ var country_list = [
 window.onload = init();
 
 function init(){
-    fill_countries_options();
-    add_footer();
+    // Get current page name
+    var page_name = window.location.pathname;
+    
+    // Using substring and lastIndexOf, we can get only the page name without the full url
+    page_name = page_name.substring(page_name.lastIndexOf("/") + 1, page_name.length);
+
+    console.log("JS Started on " + page_name);
+
+    // Add header and footer to pages
+    add_header(page_name);
+    add_footer(page_name);
+
+    if(document.getElementById("countries"))
+    {
+        fill_countries_options();
+    }
+    
 }
 
 function fill_countries_options(){
@@ -267,8 +282,51 @@ function fill_countries_options(){
     country_datalist.innerHTML = country_options;  
 }
 
-function add_footer(){
+function add_header(page_name){
+    transparent_class = "";
+    if(page_name == "home.html"){
+        transparent_class = "transparent-nav";
+    }
+    
+    html = '';
+    html += '<nav class="navbar navbar-expand-md ' + transparent_class + '">';
+    html += '<div class="container">';
+    html += '<a href="home.html">';
+    html += '<img class="navbar-brand navbar-text" src="images/logo.png" width="72px" height="auto">';
+    html += '</a>';
+    html += '<button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">';
+    html += '<span class="navbar-toggler-icon"></span>';
+    html += '</button>';
+    html += '<div class="collapse navbar-collapse" id="collapsibleNavbar">';
+    html += '<ul class="navbar-nav ms-auto">';
+    html += '<li class="nav-item"><a class="nav-link" href="home.html">Home</a></li>';
+    html += '<li class="nav-item"><a class="nav-link" href="contact-us.html">Contact</a></li>';
+    html += '</ul>';
+    html += '</div>';
+    html += '</div>';
+    html += '</nav>';
+    header_div = document.getElementById("header-div");
+    header_div.innerHTML = html;
+}
+
+function add_footer(page_name){
     html = "<div class='container-fluid'><div class='row'><div class='col text-center'>All rights reserved</div></div></div>";
     footer_div = document.getElementById("footer-div");
     footer_div.innerHTML = html;
+}
+
+function submit_inquiry_form(){
+    var fullName = document.getElementsByName("fullName")[0].value;
+    var emailAddress = document.getElementsByName("emailAddress")[0].value;
+    var phoneNumber = document.getElementsByName("phoneNumber")[0].value;
+    var text = "Dear " + fullName + ", we will call you back on " + phoneNumber + " and will send you an email on " + emailAddress;
+    alert(text);
+}
+
+function submit_contact_us_form(){
+    var fullName = document.getElementsByName("fullName")[0].value;
+    var emailAddress = document.getElementsByName("emailAddress")[0].value;
+    var phoneNumber = document.getElementsByName("phoneNumber")[0].value;
+    var text = "Dear " + fullName + ", your message has been received. We will contact you back on " + phoneNumber + " and will send you a confirmation email on " + emailAddress;
+    alert(text);
 }
